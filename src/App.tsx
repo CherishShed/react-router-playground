76a45ed08a76060616eda7c2f2142bf641fcdc23
CherishShed <c.shed2000@gmail.com>
2023-09-02 18:01:38 +0100

    created json file and started routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
+  );
 }
 
 export default App;
